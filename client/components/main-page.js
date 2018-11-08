b9026c1537ef860175760c22d2f2596d2f251380
David Jung <djung31@gmail.com>
2018-11-08 16:55:34 -0600

    switched yt to react-youtube

--- client/components/main-page.js
+++ client/components/main-page.js
@@ -1,21 +1,42 @@
-import React from 'react'
+import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {setOcrText} from '../store'
-import {YoutubePlayer} from './index';
+// import {YoutubePlayer} from './index';
+import YouTube from 'react-youtube'
 
-const MainPage = props => {
-  const {videoId, ocrText} = props;
-  const onClick = () => {
-    props.setOcrText('Lorem Ipsum');
+class MainPage extends Component {
+  onClick = () => {
+    this.props.setOcrText('Lorem Ipsum')
+  }
+  render() {
+    const {videoId, ocrText} = this.props
+    const opts = {
+      height: '360',
+      width: '640',
+      playerVars: {
+        // https://developers.google.com/youtube/player_parameters
+        autoplay: 1
+      }
+    }
+    return (
+      <div>
+        {/* <YoutubePlayer videoId={videoId}/> */}
+        <YouTube videoId={videoId} opts={opts} onReady={this._onReady} onStateChange={this._onPlayerStateChange}/>
+        <textarea value={ocrText} />
+        <button type="button" onClick={this.onClick}>
+          Lorem Ipsum
+        </button>
+      </div>
+    )
   }
 
-  return (
-    <div>
-      <YoutubePlayer videoId={videoId}/>
-      <textarea value={ocrText} />
-      <button type="button" onClick={onClick}>Lorem Ipsum</button>
-    </div>
-  )
+  _onReady(event) {
+    // access to player in all event handlers via event.target
+    event.target.pauseVideo()
+  }
+  _onPlayerStateChange(event) {
+
+  }
 }
 
 const mapState = state => ({
@@ -23,6 +44,6 @@ const mapState = state => ({
   ocrText: state.ocrText
 })
 const mapDispatch = dispatch => ({
-  setOcrText: (text) => dispatch(setOcrText(text))
+  setOcrText: text => dispatch(setOcrText(text))
 })
 export default connect(mapState, mapDispatch)(MainPage)
