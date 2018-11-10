28ca9502f729ffc7bdbc3d275829e35a2db353ed
David Jung <djung31@gmail.com>
2018-11-09 23:49:24 -0600

    weird double vision bug

--- server/api/youtube.js
+++ server/api/youtube.js
@@ -1,14 +1,17 @@
 const router = require('express').Router()
 const puppeteer = require('puppeteer')
 const axios = require('axios')
-
+const Jimp = require('jimp')
 // const {User} = require('../db/models')
 module.exports = router
 const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY
-// const HEIGHT = 360
-// const WIDTH = 640
-// const X_SCALE = 1920 / 640
-// const Y_SCALE = 1080 / 360
+
+function sleep(ms) {
+  return new Promise(resolve => {
+    setTimeout(resolve, ms)
+  })
+}
+
 // util
 
 const generateScreenshot = async (url, x, y, w, h) => {
@@ -22,35 +25,41 @@ const generateScreenshot = async (url, x, y, w, h) => {
     await page.goto(url)
     console.log('webpage: ', url)
     await page.setViewport({width: 1920, height: 1080})
-    console.log('params: ');
-    console.log(`x: ${x}`)
-    console.log(`y: ${y}`)
-    console.log(`w: ${w}`)
-    console.log(`h: ${h}`)
 
+    // await page.reload()
     const video = await page.$('.html5-video-player')
     await page.evaluate(() => {
       // Hide youtube player controls.
       let dom = document.querySelector('.ytp-chrome-bottom')
       dom.style.display = 'none'
     })
+    // await page.keyboard.press('Space') // play
+    // await page.keyboard.press('Space') // pause
+    await sleep(1000)
     // instead of saving an image we'll get .. something
-    const imageStr = await video
-      .screenshot({
-        // clip: {
-        //   x: +x,
-        //   y: +y,
-        //   width: +w,
-        //   height: +h
-        // }
-      })
-      .then(buffer => buffer.toString('base64'))
-    // console.log(imageStr);
-    await browser.close()
-    return imageStr
+    let image = await video.screenshot() // binary png?
+    browser.close();
+    return image
   } catch (err) {
     console.error(err.response)
   }
+}
+const cropImage = async (image, ...vals) => {
+  console.log('cropping...')
+  const [x, y, w, h] = vals.map(a => +a);
+  // console.log('params: ')
+  // console.log(`x: ${x}`)
+  // console.log(`y: ${y}`)
+  // console.log(`w: ${w}`)
+  // console.log(`h: ${h}`)
+  let str;
+  await Jimp.read(image).then(img => {
+    img.crop(x, y, w, h).getBase64(Jimp.AUTO, (err, res) => {
+      if (err) console.error(err.response)
+      str = res
+    })
+  })
+  return str
 }
 
 router.get('/', async (req, res, next) => {
@@ -66,20 +75,17 @@ router.get('/', async (req, res, next) => {
   const y = req.query.y
   const w = req.query.w
   const h = req.query.h
-  // console.log('params: ');
-  // console.log(`x: ${x}`)
-  // console.log(`y: ${y}`)
-  // console.log(`w: ${w}`)
-  // console.log(`h: ${h}`)
+
   const urlToScreenshot = `https://www.youtube.com/watch?v=${videoId}&t=${t}s`
   try {
     // use puppeteer to generate a screenshot of the youtube vid
-    const imageStr = await generateScreenshot(urlToScreenshot, x, y, w, h)
-    // console.log(imageStr);
-    // res.sendStatus(200);
-    // console.log(imageStr);
+    const screenshot = await generateScreenshot(urlToScreenshot)
+    const croppedImage = await cropImage(screenshot, x, y, w, h)
+    // console.log(croppedImage.slice(0, 30));
+    // await sleep(10000);
+    let imageStr = croppedImage.slice(22);
     console.log('image generated...')
-    // return
+
     // send the string to google cloud api
     // json req body
     const requestBody = {
@@ -110,7 +116,7 @@ router.get('/', async (req, res, next) => {
     })
   } catch (err) {
     console.log(err.response)
-    // next(err)
+    next(err)
   }
   // console.log('api= ', process.env.GOOGLE_API_KEY)
   // res.sendStatus(200)
