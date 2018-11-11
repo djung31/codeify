796540adfcb0014829457076a01a3a074abf2753
David Jung <djung31@gmail.com>
2018-11-11 17:58:41 -0600

    renders gif on client

--- client/components/gif-result.js
+++ client/components/gif-result.js
@@ -1,15 +1,34 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
-import {
-  resetGifmaker, generateGif
-} from '../store'
-
+import {resetGifmaker, generateGif} from '../store'
+import gifshot from 'gifshot'
 class GifResult extends Component {
   componentDidMount() {
-    this.props.generateGif(this.props.videoId);
+    this.props.generateGif(this.props.videoId)
   }
   componentWillUnmount() {
     this.props.resetGifMaker()
+  }
+  componentDidUpdate(prevProps) {
+    if (prevProps.pending !== this.props.pending) {
+      console.log('component did update')
+      gifshot.createGIF(
+        {
+          'gifWidth': 426,
+          'gifHeight': 239,
+          'frameDuration': 5,
+          images: this.props.data.map(base64=> `data:image/png;base64,${base64}`)
+        },
+        function(obj) {
+          if (!obj.error) {
+            const image = obj.image,
+            animatedImage = document.createElement('img')
+            animatedImage.src = image
+            document.body.appendChild(animatedImage)
+          }
+        }
+      )
+    }
   }
 
   render() {
@@ -18,11 +37,12 @@ class GifResult extends Component {
       <div>
         <h1>Gif Result goes here</h1>
         {pending && <h1>Loading</h1>}
-        {!pending && data.map((image, idx) => {
-          return (
-            <img key={`key ${idx}`} src={`data:image/png;base64,${image}`} />
-          )
-        })}
+        {!pending &&
+          data.map((image, idx) => {
+            return (
+              <img key={`key ${idx}`} src={`data:image/png;base64,${image}`} />
+            )
+          })}
       </div>
     )
   }
@@ -35,6 +55,6 @@ const mapState = state => ({
 })
 const mapDispatch = dispatch => ({
   resetGifmaker: () => dispatch(resetGifmaker()),
-  generateGif: (videoId) => dispatch(generateGif(videoId))
+  generateGif: videoId => dispatch(generateGif(videoId))
 })
 export default connect(mapState, mapDispatch)(GifResult)
